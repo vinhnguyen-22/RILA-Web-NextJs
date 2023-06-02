dd779883535622bc29b2bb4969fb405a75f92916
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2023-06-02 20:45:04 +0700

    Fix Header

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -30,22 +30,15 @@ const Contact = () => {
         </div>
       </div>
 
-      {/* <div className="container my-24 mx-auto md:px-6">
+      <div className="container my-24 mx-auto md:px-6">
         <section className="mb-32">
           <div className="grid grid-cols-12">
-            <div className="col-span-8">
-              <div className="h-[313px] flex flex-col items-start justify-start">
-                <Image
-                  width={667}
-                  height={0.5}
-                  className="relative w-[667px] h-0.5"
-                  alt=""
-                  src="/images/homepage/vector-6.svg"
-                />
-
-                <div className="w-[616px] flex flex-row items-center justify-start">
-                  <div className="w-[691px] flex flex-row py-8 px-2.5 box-border items-center justify-start">
-                    <div className="relative leading-[36px]">
+            <div className="sm:col-span-12 md:col-span-8 lg:col-span-8">
+              <div className="flex flex-col items-start justify-start ">
+                <div className="h-[1px] w-full bg-black"></div>
+                <div className="flex flex-row items-center justify-start">
+                  <div className="flex flex-row py-8 px-2.5 box-border items-center justify-start">
+                    <div className="relative leading-[36px] text-xl">
                       Do I need to have a social listening tool to get started with RILA?
                     </div>
                   </div>
@@ -57,17 +50,10 @@ const Contact = () => {
                     src="/images/homepage/materialsymbolsaddcirclerounded.svg"
                   />
                 </div>
-                <Image
-                  width={667}
-                  height={0.5}
-                  className="relative w-[667px] h-0.5"
-                  alt=""
-                  src="/images/homepage/vector-6.svg"
-                />
-
+                <div className="h-[1px] w-full bg-black"></div>
                 <div className="flex flex-row items-center justify-start">
                   <div className=" flex flex-row py-8 px-2.5 box-border items-center justify-start">
-                    <div className="relative leading-[36px]">
+                    <div className="relative leading-[36px] text-xl">
                       Can RILA help me choose the right in-house social listening vendor?
                     </div>
                   </div>
@@ -79,17 +65,12 @@ const Contact = () => {
                     src="/images/homepage/materialsymbolsaddcirclerounded.svg"
                   />
                 </div>
-                <Image
-                  width={667}
-                  height={0.5}
-                  className="relative w-[667px] h-0.5"
-                  alt=""
-                  src="/images/homepage/vector-6.svg"
-                />
-
-                <div className="w-[713px] flex flex-row items-center justify-start">
+                <div className="h-[1px] w-full bg-black"></div>
+                <div className="flex flex-row items-center justify-start">
                   <div className="flex-1 flex flex-row py-8 px-2.5 items-center justify-start">
-                    <div className="relative leading-[36px]">How fast can RILA deliver a social listening report?</div>
+                    <div className="relative leading-[36px] text-xl">
+                      How fast can RILA deliver a social listening report?
+                    </div>
                   </div>
                   <Image
                     className="relative w-6 h-6 overflow-hidden shrink-0"
@@ -99,31 +80,31 @@ const Contact = () => {
                     src="/images/homepage/materialsymbolsaddcirclerounded.svg"
                   />
                 </div>
-                <Image
-                  width={667}
-                  height={0.5}
-                  className="relative w-[667px] h-0.5"
-                  alt=""
-                  src="/images/homepage/vector-6.svg"
-                />
+                <div className="h-[1px] w-full bg-black"></div>
               </div>
             </div>
-            <div className="col-span-4">
+            <div className="sm:col-span-12 md:col-span-4 lg:col-span-4">
               <div className="mb-12  shrink-0 grow-0 basis-auto md:mb-0 md:w-7/12 md:px-3 lg:px-6">
                 <div className="w-full shrink-0 grow-0 basis-auto md:w-5/12 md:px-3 lg:px-6">
-                  <div className="flex flex-col items-start justify-start gap-[42px] text-21xl">
+                  <div className="flex flex-col items-start justify-start gap-[42px] text-9xl">
                     <div className="flex flex-col items-start justify-start gap-[32px]">
                       <div className="flex flex-col items-start justify-start gap-[16px]">
-                        <div className="relative text-black tracking-[0.2px] leading-[52px] capitalize inline-block w-[487px]">
+                        <div className="relative text-black tracking-[0.2px] leading-[52px] capitalize inline-block ">
                           How can We help you?
                         </div>
-                        <div className="relative text-xl leading-[36px] text-gray-500 inline-block w-[431px] opacity-[0.75] mix-blend-normal">
+                        <div className="relative text-xl leading-[36px] text-gray-500 inline-block opacity-[0.75] mix-blend-normal">
                           Contact a member of our team now to discuss your social listening needs.
                         </div>
                       </div>
                       <div className="flex flex-row items-start justify-start gap-[8px] text-base text-darkgray">
-                        <div className="rounded-18xl bg-gray-100 flex flex-row py-5 px-[18px] box-border items-center justify-start">
-                          <div className="relative capitalize">Enter your Email</div>
+                        <div className="">
+                          <input
+                            className="relative 
+                            rounded-[37px]
+                            placeholder:italic placeholder:text-slate-400
+                            capitalize rounded-18xl bg-gray-100 flex flex-row py-5 px-[18px] box-border items-center justify-start "
+                            placeholder="Enter your Email"
+                          />
                         </div>
                         <div className="rounded-51xl bg-tomato-100 w-36 h-[62px] flex flex-row py-5 px-6 box-border items-center justify-center text-white">
                           <b className="relative leading-[20px] capitalize inline-block w-[77px] shrink-0">
@@ -150,7 +131,7 @@ const Contact = () => {
             </div>
           </div>
         </section>
-      </div> */}
+      </div>
     </div>
   );
 };
