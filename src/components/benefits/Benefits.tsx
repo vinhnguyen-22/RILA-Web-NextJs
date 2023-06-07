62b047ff6b3604882447675a8c8f67309454c844
Duyen Le <lengomyduyen@gmail.com>
2023-06-07 14:36:07 -0500

    fix benefits

--- src/components/benefits/Benefits.tsx
+++ src/components/benefits/Benefits.tsx
@@ -28,24 +28,24 @@ const Benefits = () => {
   return (
     <div className="bg-gray-400 mt-[73px] py-[73px]">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
           <div className="relative md:pr-[70px]">
-            <h3 className="font-semibold text-4xl leading-[50px]  inline-block w-max-[561.16px]">
+            <h3 className="font-semibold md:text-3xl text-2xl leading-[50px] inline-block w-max-[561.16px]">
               The Benefits Of Social Listening
             </h3>
-            <div className="mt-[20px] mb-[40px] text-lg leading-[30px] text-black inline-block ">
+            <div className="md:text-lg text-sm mt-[20px] mb-[40px] text-lg leading-[30px] text-black inline-block ">
               Our social listening services help you harness actionable insights to enhance brand reputation, uncover
               market trends, and drive strategic decision-making.
             </div>
-            <div className="flex flex-col items-start justify-start gap-[20px] text-9xl">
+            <div className="flex flex-col items-start justify-start gap-[20px] text-9xl mb-12">
               {benefitsItems.map((item, index) => (
                 <div key={index} className="flex items-start justify-center gap-[27px] ">
                   <div className="w-[75%] h-[75.78px]">
                     <Image width={75} height={75.78} alt="" src={item.icon} />
                   </div>
                   <div className="flex flex-col items-start justify-start gap-[15px] ">
-                    <div className="relative leading-[38px] font-semibold inline-block">{item.title} </div>
-                    <div className="relative text-lg leading-[28px] text-black inline-block">{item.content}</div>
+                    <div className="relative md:text-xl text-lg leading-[38px] font-semibold inline-block">{item.title} </div>
+                    <div className="relative md:text-lg text-sm leading-[28px] text-black inline-block">{item.content}</div>
                   </div>
                 </div>
               ))}
