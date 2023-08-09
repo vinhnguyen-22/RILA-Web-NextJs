5fe5c9ec7864e51bbf547b3f06170f3262564ee7
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2023-08-09 08:57:01 +0700

    ix: metadata

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,13 +1,11 @@
-'use client';
+import Hero from '@/components/about/hero';
 import Heading from '@/components/common/Heading/Heading';
 import Contact from '@/components/contact/Contact';
 import AboutImg from '@/icons/about-img';
 import Polygon from '@/icons/polygon';
 import { awardImages } from '@/mocks/data';
 import { Metadata } from 'next';
-import dynamic from 'next/dynamic';
 import Image from 'next/image';
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 export const metadata: Metadata = {
   title: 'About',
@@ -38,34 +36,7 @@ function RectangleWithCircles() {
 const About = () => {
   return (
     <div className="mt-[40px]">
-      <section>
-        <div className="grid grid-cols-12 h-[527px] gap-[60px]">
-          <div className="col-span-5 h-full flex flex-col justify-center items-start gap-[15px] pl-[95px]">
-            <h1 className="text-45xl font-semibold spacing-[1.92px]">About Us</h1>
-            <p className="text-xl leading-7">
-              <strong className="text-black">Unleashing The Power Of Social Listening:</strong> Setting The Standard For
-              Data-Driven Insights
-            </p>
-            <button className="bg-red-100 px-6 py-2 text-white rounded-51xl">
-              <strong>Request A Demo</strong>
-            </button>
-          </div>
-          <div className="absolute -bottom-[65%] right-[49px] rounded-md bg-red-100 opacity-50 w-[177.5px] h-[129.248px]"></div>
-          <div className="absolute top-[60px] right-[53%] rounded-full bg-orange-200 opacity-50 w-[159.5px] h-[159.5px] "></div>
-          <div className="relative col-span-7 h-full  ">
-            <div className="h-full overflow-hidden rounded-bl-[178px] bg-slate-700">
-              <ReactPlayer
-                height={'100%'}
-                width={'100%'}
-                className=""
-                url="https://www.youtube.com/watch?v=38fpciTC6JA"
-                controls={false}
-                light={true}
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      <Hero />
       <section className="section mx-[95px] ">
         <div className="grid grid-cols-12 h-[527px] gap-[60px]">
           <div className="relative max-sm:hidden col-span-5 h-full flex-col justify-center items-center">
