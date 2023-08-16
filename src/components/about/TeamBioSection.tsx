dda9b9757461d372d7e29619f8529942dcd71563
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2023-08-17 00:23:05 +0700

    feat: slide teambio dynamic bullets

--- src/components/about/TeamBioSection.tsx
+++ src/components/about/TeamBioSection.tsx
@@ -9,14 +9,45 @@ import TeamDot from '@/icons/about/TeamDot';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 
 const TeamBioSection = () => {
+  const media = useMediaQuery('(max-width: 800px)');
+  console.log(media);
   const [indexBio, setIndexBio] = useState<number>(0);
   const swiperRef = useRef<any>();
-  const media = useMediaQuery('(max-width: 600px)');
   useEffect(() => {
     if (indexBio == swiperRef.current.activeIndex) {
       swiperRef.current.slideTo(indexBio);
     }
   }, [indexBio]);
+
+  const RenderSlideContent = () => (
+    <div className="lg:col-span-1 col-span-2 flex justify-center items-center">
+      <Swiper
+        slidesPerView={1}
+        grabCursor={true}
+        modules={[Navigation, Pagination]}
+        onSlideChange={(e) => {
+          setIndexBio(e.realIndex);
+        }}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        className="teamBioSwiper"
+        pagination={{ clickable: true, dynamicBullets: true }}
+        loop
+        init
+        centeredSlides={true}
+      >
+        {AboutTeamBio.map((item, index) => (
+          <SwiperSlide key={index} className="">
+            <div className="flex justify-center items-center flex-col h-full gap-[20px]">
+              <h3 className="font-semibold text-3xl">{item.name}</h3>
+              <p className="text-black">{item.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
   return (
     <section className="mt-[100px] mx-[10px]">
       <div className="flex flex-col justify-center items-center sm:text-left text-center">
@@ -40,41 +71,16 @@ const TeamBioSection = () => {
               }}
             ></div>
           </div>
-
-          <div className="lg:flex hidden justify-center items-center">
-            <Swiper
-              slidesPerView={1}
-              grabCursor={true}
-              modules={[Navigation, Pagination]}
-              onSlideChange={(e) => {
-                setIndexBio(e.realIndex);
-              }}
-              onSwiper={(swiper) => {
-                swiperRef.current = swiper;
-              }}
-              className="teamBioSwiper"
-              pagination={{ clickable: true }}
-              loop
-              init
-            >
-              {AboutTeamBio.map((item, index) => (
-                <SwiperSlide key={index} className="">
-                  <div className="flex justify-center items-center flex-col h-full gap-[20px]">
-                    <h3 className="font-semibold text-3xl">{item.name}</h3>
-                    <p className="text-black">{item.description}</p>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-
+          {!media ? RenderSlideContent() : <></>}
           <div className="flex justify-center items-center flex-wrap sm:gap-[20px] gap-[10px] p-2 lg:mb-0 mb-12">
             {AboutTeamBio.map(
               (item, index) =>
                 index != indexBio && (
                   <div
-                    className="sm:w-[84px] w-[50px] sm:h-[84px] h-[50px] rounded-full relative overflow-hidden hover:scale-110 shadow-md"
-                    onClick={() => setIndexBio(index)}
+                    className="cursor-pointer sm:w-[84px] w-[50px] sm:h-[84px] h-[50px] rounded-full relative overflow-hidden hover:scale-110 hover:opacity-85 shadow-md"
+                    onClick={() => {
+                      setIndexBio(index);
+                    }}
                     key={index}
                   >
                     <Image
@@ -88,33 +94,7 @@ const TeamBioSection = () => {
                 )
             )}
           </div>
-
-          <div className="lg:hidden flex justify-center items-center col-span-2 p-3">
-            <Swiper
-              slidesPerView={1}
-              grabCursor={true}
-              modules={[Navigation, Pagination]}
-              onSlideChange={(e) => {
-                setIndexBio(e.realIndex);
-              }}
-              onSwiper={(swiper) => {
-                swiperRef.current = swiper;
-              }}
-              className="teamBioSwiper"
-              pagination={{ clickable: true }}
-              loop
-              init
-            >
-              {AboutTeamBio.map((item, index) => (
-                <SwiperSlide key={index} className="">
-                  <div className="flex justify-center items-center flex-col h-full gap-[20px]">
-                    <h3 className="font-semibold text-3xl">{item.name}</h3>
-                    <p className="text-black">{item.description}</p>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+          {!!media ? RenderSlideContent() : <></>}
         </div>
       </div>
     </section>
