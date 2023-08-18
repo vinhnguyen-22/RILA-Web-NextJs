495f7a5699cd7ed8569866b621b625bd09626418
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2023-08-18 17:08:17 +0700

    fix: case study dummy text

--- src/app/casestudy/page.tsx
+++ src/app/casestudy/page.tsx
@@ -23,9 +23,7 @@ const CaseStudyPage = async () => {
         <div className="flex flex-col mx-auto container mt-[100px]">
           <Heading
             title="Case Studies"
-            subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book."
+            subtitle="Discover our comprehensive case studies, showcasing RILA’s global leadership in social listening. Explore how our data-driven insights empower clients to achieve transformative results through cutting-edge solutions."
           />
         </div>
 
--- src/components/report/ReportDetail.tsx
+++ src/components/report/ReportDetail.tsx
@@ -118,9 +118,7 @@ const ReportDetail: FC<ReportDetailProps> = ({ report }) => {
 
                     <div className="inline-block relative w-64">
                       <select className="block appearance-none w-full border-b border-gray-500 hover:border-gray-500 px-4 py-2 pr-8 leading-tight focus:outline-none focus:shadow-outline">
-                        <option>Really long</option>
-                        <option>Option 2</option>
-                        <option>Option 3</option>
+                        <option>-- Choose country</option>
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
