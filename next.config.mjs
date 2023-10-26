8a88158393bee2a100af7c50b4d4b6421aa948cc
duyenle1312 <duyenle131202@gmail.com>
2023-10-26 16:14:25 -0700

    unoptimise images

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   swcMinify: true,
   staticPageGenerationTimeout: 300,
   images: {
+    unoptimized: true,
     domains: [
       'www.notion.so',
       's3.us-west-2.amazonaws.com',
