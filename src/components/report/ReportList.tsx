1d091af73f3b42e87dfc8a55392f6dd44c17b031
duyenle1312 <duyenle131202@gmail.com>
2023-12-07 22:44:02 +0200

    add filter

--- src/components/report/ReportList.tsx
+++ src/components/report/ReportList.tsx
@@ -2,7 +2,7 @@
 
 import useReports from '@/hooks/useReports';
 import { Report } from '@/types/cms';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Chip } from '../common/Chip';
 import Paginate from '../common/Paginate/Paginate';
 import SearchBar from '../common/SearchBar/SearchBar';
@@ -13,27 +13,43 @@ type Props = {
 
 const ReportList = ({ data }: Props) => {
   const { reports, totalPages } = useReports(data);
+  const [filteredReport, setFilteredReport] = useState(reports);
   const rootRef = useRef<HTMLDivElement>(null);
   const tags = [...new Set(data.map((item) => item.tags && item.tags[0].name))];
+
+  const filterTag = (tag: string) => {
+    if (tag == 'All') {
+      setFilteredReport(reports);
+    } else {
+      setFilteredReport(reports.filter((r) => r.tags[0].name == tag));
+    }
+  };
 
   return (
     <div ref={rootRef} className="">
       <div className="grid grid-cols-1">
         <div className="flex flex-row flex-wrap justify-center items-center text-gray-800 gap-[15px] my-[50px]">
-          {tags.map((t, index) => (
-            <Chip
-              key={index}
-              className="cursor-pointer px-[15px] py-[4px] text-lg font-semibold uppercase hover:text-white rounded-2xl border-gray-800 border-[1px] hover:bg-navy-100 "
-            >
-              {t}
+          <div onClick={() => filterTag('All')}>
+            <Chip className="cursor-pointer px-[15px] py-[4px] text-md font-semibold hover:text-white rounded-2xl border-gray-800 border-[1px] hover:bg-navy-100 ">
+              All
             </Chip>
+          </div>
+          {tags.map((t, index) => (
+            <div key={index} onClick={() => filterTag(t || '')}>
+              <Chip
+                key={index}
+                className="cursor-pointer px-[15px] py-[4px] text-md font-semibold hover:text-white rounded-2xl border-gray-800 border-[1px] hover:bg-navy-100 "
+              >
+                {t}
+              </Chip>
+            </div>
           ))}
         </div>
         <SearchBar className="lg:w-1/2 mx-auto" />
       </div>
 
       <div className="mx-1 my-5 grid grid-cols-1 md:grid-cols-3 max-sm:gap-[8px] gap-[58px] ">
-        {reports.map((element, i) => (
+        {filteredReport.map((element, i) => (
           <ReportCard key={i} {...element} />
         ))}
       </div>
